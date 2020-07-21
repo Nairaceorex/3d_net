95b32ece07fb07b39aa9edbe111501a63c654f8d
ASUS <Nairaceorex>
2020-07-21 21:34:26 +0300

    Add file

---
+++ neuron.py
@@ -0,0 +1,27 @@
+import torch
+
+
+class Net(torch.nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = torch.nn.Linear(3, 64)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        return x
+
+
+x = torch.randn([1, 100, 3])
+target = torch.randn([1, 100, 64])
+
+model = Net()
+criterian = torch.nn.MSELoss()
+optimazer = torch.optim.SGD(model.parameters(), lr=0.0003)
+
+for epoch in range(10):
+    output = model(x)
+    loss = criterian(output, target)
+    print("Epoch:", epoch, "Loss:", loss)
+    optimazer.zero_grad()
+    loss.backward()
+    optimazer.step()
